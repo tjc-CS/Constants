3ae674e6a440f81946e65567faf5a2e1d5af6048
Tony Collins <tjc@eltham-college.org.uk>
2024-06-11 15:02:47 +0100

    new change

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp12
      
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello year 12");
+            Console.WriteLine("Hello year 12 +year 13");
             Console.WriteLine(Cons.MAX_PLAYERS + Cons.NUMBER_OF_CARDS);
             Cons.print();
             int[] cards = new int[Cons.NUMBER_OF_CARDS];
